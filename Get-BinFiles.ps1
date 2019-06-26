ccec5bfb1678d43065145ee2bf894460239831ee
eSecRPM <esecrpm@users.noreply.github.com>
2019-06-26 21:29:31 +0000

    Minor updates
    Removed 7zip dependency and added support for PS1 files

--- Get-BinFiles.ps1
+++ Get-BinFiles.ps1
@@ -1,24 +1,30 @@
 <#
     .SYNOPSIS
-    Ths script will download binaries identified in each KAPE module and extract them to the bin directory. It is expected this script is run from the KAPE\modules directory. This script requires 7-zip binaries to be placed in a 7z folder within the root of the KAPE directory.
+    This script will downloads third party binaries required for KAPE modules and places them in the bin directory.
     .DESCRIPTION
-    This script will attempt to download third party binaries required for KAPE modules and place them in the bin directory.
+    This script downloads EXE, PS1, and ZIP files identified in the BinaryUrl field of KAPE modules
+    and extracts archives to the bin directory.
+    
+    To use, place this script in the KAPE\modules directory.
     .EXAMPLE
     C:\PS> Get-BinFiles.ps1 
     
     .NOTES
     Author: eSecRPM
-    Date:   2019-03-21    
+    Date:   2019-03-21
+    Update: 2019-06-26  Removed 7zip dependency and added logic for PS1 extensions
 #>
 
 # The following modules do not contain a BinaryUrl field that points directly to a downloadable
-# zip file or exe.  Binaries for these modules will need to be downloaded manually.
+# script, exe, or zip file.  Binaries for these modules will need to be downloaded manually.
 #
 # ApplicationEvents.mkape
 # ApplicationFullEventLogView.mkape
 # Detailed-Network-Share-Access.mkape
-# exiftool.mkape
+# DumpIt.mkape
+# Hashes.mkape
 # Hindsight.mkape
+# iTunesBackup.mkape
 # Logon-Logoff-events.mkape
 # Plaso.mkape
 # PowerShell.mkape
@@ -28,44 +34,40 @@
 # RDP-Usage-events.mkape
 # Registry_System.mkape
 # SecurityEvents.mkape
+# SparkCore.mkape
 # SystemEvents.mkape
 # SystemFullEventLogView.mkape
 # TaskScheduler.mkape
 # TS-LSM.mkape
 # TS-RCM.mkape
+# usbdeviceforensics.mkape
 
 
 
 $currentDirectory = (Resolve-Path ".")
 $destinationDir = [string]$currentDirectory+"\bin\"
 
-if (!(test-path "..\7z\7za.exe")){
-	Write-Host "`n..\7z\7za.exe needed! Exiting`n" -BackgroundColor Red
-	return
-} 
-set-alias sz "..\7z\7za.exe"
-
 Get-ChildItem -Path $currentDirectory -Filter *.mkape | Get-Content | ForEach-Object {
 	$items = $_.split()
 	if ($items[0] -eq "BinaryUrl:"){
 		$URL = [string]$items[1]
 		$filename = Split-Path $URL -Leaf
-		$filename | ForEach-Object {
-			$segments = $_.split('.')
-			$extension = [string]$segments[1]
-		}
-		
-		Echo $URL
+        $start = $filename.lastIndexOf('.')+1
+        $len = $filename.length
+        $extension = $filename.Substring($start,$len-$start)
+        
+        Echo $URL
 		
 		if ($extension -eq "exe"){(New-Object System.Net.WebClient).DownloadFile($url,"$destinationDir$filename")}
+		if ($extension -eq "ps1"){(New-Object System.Net.WebClient).DownloadFile($url,"$destinationDir$filename")}
 		if ($extension -eq "zip"){
 			(New-Object System.Net.WebClient).DownloadFile($url,"$destinationDir$filename")
-			sz x $destinationDir$filename -o"$destinationDir" -y > $null
+            Expand-Archive -Path $destinationDir$filename -DestinationPath $destinationDir -Force
 			Remove-Item -Path $destinationDir$filename
 		}
 	}
 }
-		
+
 # densityscout, RECmd, SBECmd, sqlite3
 Copy-Item -Path $destinationDir"win64\densityscout.exe" -Destination $destinationDir -Force
 Copy-Item -Path $destinationDir"RegistryExplorer\RECmd.exe" -Destination $destinationDir -Force
