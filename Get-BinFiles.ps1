651a93171fdac0947b14feab704669086f14c907
eSecRPM <esecrpm@users.noreply.github.com>
2019-06-26 21:46:58 +0000

    Minor update
    Added logic to download LogParser.msi

--- Get-BinFiles.ps1
+++ Get-BinFiles.ps1
@@ -1,18 +1,18 @@
 <#
-    .SYNOPSIS
-    This script will downloads third party binaries required for KAPE modules and places them in the bin directory.
-    .DESCRIPTION
-    This script downloads EXE, PS1, and ZIP files identified in the BinaryUrl field of KAPE modules
-    and extracts archives to the bin directory.
-    
-    To use, place this script in the KAPE\modules directory.
-    .EXAMPLE
-    C:\PS> Get-BinFiles.ps1 
-    
-    .NOTES
-    Author: eSecRPM
-    Date:   2019-03-21
-    Update: 2019-06-26  Removed 7zip dependency and added logic for PS1 extensions
+	.SYNOPSIS
+	This script will downloads third party binaries required for KAPE modules and places them in the bin directory.
+	.DESCRIPTION
+	This script downloads EXE, PS1, and ZIP files identified in the BinaryUrl field of KAPE modules
+	and extracts archives to the bin directory.
+	
+	To use, place this script in the KAPE\modules directory.
+	.EXAMPLE
+	C:\PS> Get-BinFiles.ps1 
+	
+	.NOTES
+	Author: eSecRPM
+	Date:   2019-03-21
+	Update: 2019-06-26  Removed 7zip dependency and added logic for PS1 extensions, LogParser.msi
 #>
 
 # The following modules do not contain a BinaryUrl field that points directly to a downloadable
@@ -20,18 +20,15 @@
 #
 # ApplicationEvents.mkape
 # ApplicationFullEventLogView.mkape
-# Detailed-Network-Share-Access.mkape
 # DumpIt.mkape
 # Hashes.mkape
 # Hindsight.mkape
 # iTunesBackup.mkape
-# Logon-Logoff-events.mkape
 # Plaso.mkape
 # PowerShell.mkape
 # PowershellOperationalFullEventLogView.mkape
 # PrintServiceOperationalFullEventLogView.mkape
 # RDPCoreTS.mkape
-# RDP-Usage-events.mkape
 # Registry_System.mkape
 # SecurityEvents.mkape
 # SparkCore.mkape
@@ -52,18 +49,22 @@ Get-ChildItem -Path $currentDirectory -Filter *.mkape | Get-Content | ForEach-Ob
 	if ($items[0] -eq "BinaryUrl:"){
 		$URL = [string]$items[1]
 		$filename = Split-Path $URL -Leaf
-        $start = $filename.lastIndexOf('.')+1
-        $len = $filename.length
-        $extension = $filename.Substring($start,$len-$start)
-        
-        Echo $URL
+		$start = $filename.lastIndexOf('.')+1
+		$len = $filename.length
+		$extension = $filename.Substring($start,$len-$start)
+		
+		Echo $URL
 		
 		if ($extension -eq "exe"){(New-Object System.Net.WebClient).DownloadFile($url,"$destinationDir$filename")}
 		if ($extension -eq "ps1"){(New-Object System.Net.WebClient).DownloadFile($url,"$destinationDir$filename")}
 		if ($extension -eq "zip"){
 			(New-Object System.Net.WebClient).DownloadFile($url,"$destinationDir$filename")
-            Expand-Archive -Path $destinationDir$filename -DestinationPath $destinationDir -Force
+			Expand-Archive -Path $destinationDir$filename -DestinationPath $destinationDir -Force
 			Remove-Item -Path $destinationDir$filename
+		}
+		if ($extension -eq "aspx?id=24659"){
+			$filename = "LogParser.msi"
+			(New-Object System.Net.WebClient).DownloadFile($url,"$destinationDir$filename")
 		}
 	}
 }
