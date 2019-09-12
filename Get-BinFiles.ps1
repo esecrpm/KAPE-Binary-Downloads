26776f722122bf3f24ecde79582a5194881ffb12
eSecRPM <esecrpm@users.noreply.github.com>
2019-09-12 13:26:35 -0400

    Sub-directory support added
    Added support for module sub-directory structure added in KAPE 0.8.7.0

--- Get-BinFiles.ps1
+++ Get-BinFiles.ps1
@@ -11,43 +11,37 @@
 	The following modules do not contain a BinaryUrl field that points directly to a downloadable
 	script, exe, or zip file.  Binaries for these modules will need to be downloaded manually.
 	
-		Apache_Access_Log.mkape:6:BinaryUrl: https://www.microsoft.com/en-us/download/confirmation.aspx?id=24659
-		ApplicationEvents.mkape:6:BinaryUrl: https://tzworks.net/download_links.php
-		ApplicationFullEventLogView.mkape:6:BinaryUrl: https://www.nirsoft.net/utils/full_event_log_view.html
-		Detailed-Network-Share-Access.mkape:6:BinaryUrl: https://www.microsoft.com/en-us/download/confirmation.aspx?id=24659
-		DumpIt.mkape:6:BinaryUrl: https://my.comae.io
-		Hashes.mkape:6:BinaryUrl: https://github.com/gentilkiwi/mimikatz/releases
-		Hindsight.mkape:6:BinaryUrl: https://github.com/obsidianforensics/hindsight/releases
-		iTunesBackup.mkape:6:BinaryUrl: https://github.com/jfarley248/iTunes_Backup_Analyzer/releases
-		Logon-Logoff-events.mkape:6:BinaryUrl: https://www.microsoft.com/en-us/download/confirmation.aspx?id=24659
-		Plaso.mkape:6:BinaryUrl: https://github.com/log2timeline/plaso/releases
-		PowerShell.mkape:6:BinaryUrl: https://tzworks.net/download_links.php
-		PowershellOperationalFullEventLogView.mkape:6:BinaryUrl: https://www.nirsoft.net/utils/full_event_log_view.html
-		PrintServiceOperationalFullEventLogView.mkape:6:BinaryUrl: https://www.nirsoft.net/utils/full_event_log_view.html
-		RDP-Usage-events.mkape:6:BinaryUrl: https://www.microsoft.com/en-us/download/confirmation.aspx?id=24659
-		RDPCoreTS.mkape:6:BinaryUrl: https://tzworks.net/download_links.php
-		Registry_System.mkape:6:BinaryUrl: https://tzworks.net/download_links.php
-		RegRipper-ALL.mkape:6:BinaryUrl: https://github.com/keydet89/RegRipper2.8
-		RegRipper-ntuser.mkape:6:BinaryUrl: https://github.com/keydet89/RegRipper2.8
-		RegRipper-sam.mkape:6:BinaryUrl: https://github.com/keydet89/RegRipper2.8
-		RegRipper-security.mkape:6:BinaryUrl: https://github.com/keydet89/RegRipper2.8
-		RegRipper-software.mkape:6:BinaryUrl: https://github.com/keydet89/RegRipper2.8
-		RegRipper-system.mkape:6:BinaryUrl: https://github.com/keydet89/RegRipper2.8
-		ScheduledTasksFullEventLogView.mkape:6:BinaryUrl: https://www.nirsoft.net/utils/full_event_log_view.html
-		SecurityEvents.mkape:6:BinaryUrl: https://tzworks.net/download_links.php
-		SecurityFullEventLogView.mkape:6:BinaryUrl: https://www.nirsoft.net/utils/full_event_log_view.html
-		SMB-Server-Anonymous-Logon.mkape:6:BinaryUrl: https://www.microsoft.com/en-us/download/confirmation.aspx?id=24659
-		SparkCore.mkape:6:BinaryUrl: https://www.nextron-systems.com/spark-core/
-		Symantec_Control_Log.mkape:6:BinaryUrl: https://www.microsoft.com/en-us/download/confirmation.aspx?id=24659
-		Symantec_Daily_AV.mkape:6:BinaryUrl: https://www.microsoft.com/en-us/download/confirmation.aspx?id=24659
-		Symantec_System_Log.mkape:6:BinaryUrl: https://www.microsoft.com/en-us/download/confirmation.aspx?id=24659
-		Symantec_Tamper_Protection_Log.mkape:6:BinaryUrl: https://www.microsoft.com/en-us/download/confirmation.aspx?id=24659
-		SystemEvents.mkape:6:BinaryUrl: https://tzworks.net/download_links.php
-		SystemFullEventLogView.mkape:6:BinaryUrl: https://www.nirsoft.net/utils/full_event_log_view.html
-		TaskScheduler.mkape:6:BinaryUrl: https://tzworks.net/download_links.php
-		TS-LSM.mkape:6:BinaryUrl: https://tzworks.net/download_links.php
-		TS-RCM.mkape:6:BinaryUrl: https://tzworks.net/download_links.php
-		usbdeviceforensics.mkape:6:BinaryUrl: https://github.com/woanware/usbdeviceforensics
+	BrowsingHistory\Hindsight.mkape:6:BinaryUrl: https://github.com/obsidianforensics/hindsight/releases
+	EventLogs\ApplicationFullEventLogView.mkape:6:BinaryUrl: https://www.nirsoft.net/utils/full_event_log_view.html
+	EventLogs\Detailed-Network-Share-Access.mkape:6:BinaryUrl: https://www.microsoft.com/en-us/download/confirmation.aspx?id=24659
+	EventLogs\Logon-Logoff-events.mkape:6:BinaryUrl: https://www.microsoft.com/en-us/download/confirmation.aspx?id=24659
+	EventLogs\PowershellOperationalFullEventLogView.mkape:6:BinaryUrl: https://www.nirsoft.net/utils/full_event_log_view.html
+	EventLogs\PrintServiceOperationalFullEventLogView.mkape:6:BinaryUrl: https://www.nirsoft.net/utils/full_event_log_view.html
+	EventLogs\RDP-Usage-events.mkape:6:BinaryUrl: https://www.microsoft.com/en-us/download/confirmation.aspx?id=24659
+	EventLogs\ScheduledTasksFullEventLogView.mkape:6:BinaryUrl: https://www.nirsoft.net/utils/full_event_log_view.html
+	EventLogs\SecurityFullEventLogView.mkape:6:BinaryUrl: https://www.nirsoft.net/utils/full_event_log_view.html
+	EventLogs\SMB-Server-Anonymous-Logon.mkape:6:BinaryUrl: https://www.microsoft.com/en-us/download/confirmation.aspx?id=24659
+	EventLogs\SystemFullEventLogView.mkape:6:BinaryUrl: https://www.nirsoft.net/utils/full_event_log_view.html
+	LiveResponse\DumpIt.mkape:6:BinaryUrl: https://my.comae.io
+	LiveResponse\Hashes.mkape:6:BinaryUrl: https://github.com/gentilkiwi/mimikatz/releases
+	Misc\Apache_Access_Log.mkape:6:BinaryUrl: https://www.microsoft.com/en-us/download/confirmation.aspx?id=24659
+	Misc\iTunesBackup.mkape:6:BinaryUrl: https://github.com/jfarley248/iTunes_Backup_Analyzer/releases
+	Misc\SEPM_Logs.mkape:6:BinaryUrl: https://github.com/Beercow/SEPparser/releases
+	Misc\SparkCore.mkape:6:BinaryUrl: https://www.nextron-systems.com/spark-core/
+	Misc\TaskScheduler.mkape:6:BinaryUrl: https://tzworks.net/download_links.php
+	Registry\Registry_System.mkape:6:BinaryUrl: https://tzworks.net/download_links.php
+	Registry\RegRipper-ALL.mkape:6:BinaryUrl: https://github.com/keydet89/RegRipper2.8
+	Registry\RegRipper-ntuser.mkape:6:BinaryUrl: https://github.com/keydet89/RegRipper2.8
+	Registry\RegRipper-sam.mkape:6:BinaryUrl: https://github.com/keydet89/RegRipper2.8
+	Registry\RegRipper-security.mkape:6:BinaryUrl: https://github.com/keydet89/RegRipper2.8
+	Registry\RegRipper-software.mkape:6:BinaryUrl: https://github.com/keydet89/RegRipper2.8
+	Registry\RegRipper-system.mkape:6:BinaryUrl: https://github.com/keydet89/RegRipper2.8
+	Timelining\Convert_unicode.mkape:6:BinaryUrl: https://github.com/mdegrazia/KAPE_Tools
+	Timelining\EvtxECmd_to_TLN.mkape:6:BinaryUrl: https://github.com/mdegrazia/KAPE_Tools
+	Timelining\RegRipper_AppCompatCache_TLN.mkape:6:BinaryUrl: https://github.com/keydet89/RegRipper2.8
+	Timelining\RegRipper_NTUSER_muicache_TLN.mkape:6:BinaryUrl: https://github.com/keydet89/RegRipper2.8
+	Timelining\RegRipper_NTUSER_userassit_TLN.mkape:6:BinaryUrl: https://github.com/keydet89/RegRipper2.8
+	Timelining\RegRipper_Services_TLN.mkape:6:BinaryUrl: https://github.com/keydet89/RegRipper2.8
 
 	.EXAMPLE
 	C:\PS> Get-BinFiles.ps1 
@@ -60,14 +54,15 @@
 	2019-06-26  Removed 7zip dependency and added logic for PowerShell scripts
 	2019-06-27	Moved list of modules without executable download links to description
 				Select-String -Pattern BinaryUrl *.mkape | Where-Object { !( $_ | Select-String -Pattern "(.zip)|(.exe)|(.ps1)" -quiet) }
-	
+	2019-09-12	Updated to support sub-directory structure of KAPE modules as of 0.8.7.0
+				Get-ChildItem -Recurse *.mkape | Select-String -Pattern BinaryUrl | Where-Object { !( $_ | Select-String -Pattern "(.zip)|(.exe)|(.ps1)" -quiet) }
 #>
 
 
 $currentDirectory = (Resolve-Path ".")
 $destinationDir = [string]$currentDirectory+"\bin\"
 
-Get-ChildItem -Path $currentDirectory -Filter *.mkape | Get-Content | ForEach-Object {
+Get-ChildItem -Path $currentDirectory -Recurse -Filter *.mkape | Get-Content | ForEach-Object {
 	$items = $_.split()
 	if ($items[0] -eq "BinaryUrl:"){
 		$URL = [string]$items[1]
